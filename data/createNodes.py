176dcf71470bc1445ae5b4983309d29f994d05ad
ArhostCode <e-gulyakin@mail.ru>
2022-04-02 17:34:31 +0300

    Added map.html

--- data/createNodes.py
+++ data/createNodes.py
@@ -18,13 +18,13 @@ for k in all:
 nodes = []
 for z in a:
     if z in allMax:
-        nodes.append({'id': z, 'fill': {'src': allMax[z]['img']}, "url":"mangalib.me/"+allMax[z]['name']})
+        nodes.append({'id': z, 'shape': 'circularImage','label':z, "image":allMax[z]['img']})
     else:
         if z in allEn:
-            nodes.append({'id': z, 'url':"mangalib.me/"+allEn[z]})
+            nodes.append({'id': z, 'shape': 'circularImage','label':z, "image":'camera.jpg'})
         else:
-            nodes.append({'id': z, 'url': "mangalib.me/"})
+            nodes.append({'id': z, 'shape': 'circularImage','label':z, "image":'camera.jpg'})
 
 t = {'nodes':nodes,"edges":r}
 
-print(json.dumps(t, ensure_ascii=False))
+print(json.dumps(nodes, ensure_ascii=False))
